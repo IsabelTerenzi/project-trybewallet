b80338b28d3881911bf46aa7fc089c1c54cde73b
Isabel Terenzi <bel.terenzi@gmail.com>
2022-06-22 17:57:49 -0300

    requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,87 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
+import { actionLogin } from '../actions/index';
 
 class Login extends React.Component {
+  state = {
+    isButtonDisabled: true,
+    email: '',
+    senha: '',
+  }
+
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value },
+      () => {
+        this.verificaBotao();
+      });
+  }
+
+  verificaBotao = () => {
+    const { email, senha } = this.state;
+    const numMin = 6;
+    if (senha.length < numMin
+      || !email.includes('@')
+      || !email.includes('.com')) {
+      this.setState({ isButtonDisabled: true });
+    } else {
+      this.setState({ isButtonDisabled: false });
+    }
+  }
+
+  botaoEntrar = () => {
+    const { dispatch, history } = this.props;
+    const { email } = this.state;
+    dispatch(actionLogin(email));
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const { isButtonDisabled, email, senha } = this.state;
+    return (
+      <div>
+        <form>
+          <label htmlFor="email">
+            Email:
+            <input
+              type="email"
+              id="email"
+              data-testid="email-input"
+              name="email"
+              onChange={ this.onInputChange }
+              value={ email }
+            />
+          </label>
+          <label htmlFor="senha">
+            Senha:
+            <input
+              type="senha"
+              id="senha"
+              data-testid="password-input"
+              name="senha"
+              onChange={ this.onInputChange }
+              value={ senha }
+            />
+          </label>
+          <button
+            type="button"
+            disabled={ isButtonDisabled }
+            onClick={ this.botaoEntrar }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect()(Login);
